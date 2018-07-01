a4111bca2cfda786049df6d500c3330815ad7ca5
Ubuntu <nmikuni@dev-website.4vofaccazpru3n0rh0kfaobwjb.ix.internal.cloudapp.net>
2018-07-01 06:02:57 +0000

    edit route.rb

--- config/routes.rb
+++ config/routes.rb
@@ -2,13 +2,13 @@ Rails.application.routes.draw do
   root to: 'welcome#index'
   get 'welcome/index'
   resources :drink
-  resources :cold-starter
-  resources :warm-starter
+  resources :coldstarter
+  resources :warmstarter
   resources :soup
   resources :seafood
   resources :main
   resources :dessert
-  resources :kn-manage
+  resources :knmanage
 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
